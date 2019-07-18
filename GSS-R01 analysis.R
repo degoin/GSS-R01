26976c984e7928005082682cb19bc3c978d10329
Dana Goin <danaegoin@gmail.com>
2019-07-18 15:16:40 -0700

    Test commit

--- GSS-R01 analysis.R
+++ GSS-R01 analysis.R
@@ -6,4 +6,5 @@ df_all <- read.csv("/Users/danagoin/Documents/R01 GSS New Methods/Data/R01_150_m
 
 # see if there are any protocols or descriptions of what was done 
 # otherwise double-check that the two replications of the samples should be averaged 
-# then reshape and merge on demographics from the data Lynn sent 
+# then reshape and merge on demographics from the CiOB-Echo data 
+
