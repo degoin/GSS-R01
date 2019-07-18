95917f6e1e558680bd6242c702be391b06a5ca87
Dana Goin <danaegoin@gmail.com>
2019-07-18 15:13:47 -0700

    Script to read in and analyze the GSS-R01 data

---
+++ GSS-R01 analysis.R
@@ -0,0 +1,9 @@
+
+
+# this is the data that Dimitri has cleaned 
+# the rows are the chemical features and the columns are the samples 
+df_all <- read.csv("/Users/danagoin/Documents/R01 GSS New Methods/Data/R01_150_maternal_and_cord_1.2.csv")
+
+# see if there are any protocols or descriptions of what was done 
+# otherwise double-check that the two replications of the samples should be averaged 
+# then reshape and merge on demographics from the data Lynn sent 
